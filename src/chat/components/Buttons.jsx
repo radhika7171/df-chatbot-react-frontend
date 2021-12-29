b63b85d151a9712aa89236b71455dcf8245ff964
radhika7171 <aairamahajan@gmail.com>
2021-12-29 16:08:53 -0500

    saved selected button option from button widget

--- src/chat/components/Buttons.jsx
+++ src/chat/components/Buttons.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import axios from "axios";
 import "./Chips.css";
 import "./Buttons.css";
 
 const Buttons = (props) => {
   const { buttons } = props.widgetConfig;
-  let savedOption = null;
+  let message = null;
 
   return (
     <div className="button-comp">
@@ -13,7 +14,8 @@ const Buttons = (props) => {
           className="button"
           key={index}
           onClick={() => {
-            savedOption = option.text.stringValue;
+            message = option.text.stringValue;
+            props.actionProvider.fetcheduserDataFromNode(message);
           }}
         >
           {option.text.stringValue.toUpperCase()}
